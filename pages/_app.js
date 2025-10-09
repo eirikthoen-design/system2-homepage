fbd1ba5a65fa3698b319d38cc66830cdbb7b07af
eirikthoen-design <eirikthoen@gmail.com>
2025-10-09 06:55:22 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '../styles/global.css';
-import { Inter } from 'next/font/google';
-
-// Explicitly load weights 300 and 400 to match Lovable's light heading + normal body
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['300','400'],
-  variable: '--font-inter',
-  display: 'swap',
-});
 
 export default function MyApp({ Component, pageProps }) {
-  return <main className={inter.variable}><Component {...pageProps} /></main>;
+  return <Component {...pageProps} />;
 }
