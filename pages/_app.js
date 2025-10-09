adeded4ce4e76fb4aae300c37c7053c2336b2c9e
eirikthoen-design <eirikthoen@gmail.com>
2025-10-09 06:38:06 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
-
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <main className={`${inter.variable} antialiased font-sans`}><Component {...pageProps} /></main>;
 }
