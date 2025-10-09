70d6b3d5bffa0dc4f60cac32e116ad1321a8c02f
eirikthoen-design <eirikthoen@gmail.com>
2025-10-09 06:41:49 +0200

    Delete pages directory

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/globals.css';
-import { Inter } from 'next/font/google';
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
-export default function MyApp({ Component, pageProps }) {
-  return <main className={`${inter.variable} antialiased font-sans`}><Component {...pageProps} /></main>;
-}
